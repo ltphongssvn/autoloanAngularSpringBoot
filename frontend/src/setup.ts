85e78cda04279bc2afc3c322a52b75af9687c89a
Thanh Phong Le <ltphongssvn@gmail.com>
2026-02-24 22:41:30 -0800

    feat: add Angular auth core with tests - service, interceptor, guard, setup

---
+++ frontend/src/setup.ts
@@ -0,0 +1,11 @@
+import '@angular/compiler';
+import 'zone.js';
+import 'zone.js/testing';
+import { TestBed } from '@angular/core/testing';
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+
+TestBed.initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting(),
+  { teardown: { destroyAfterEach: true } }
+);
