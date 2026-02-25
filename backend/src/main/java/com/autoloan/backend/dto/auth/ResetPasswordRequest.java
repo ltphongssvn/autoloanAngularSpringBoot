37c906bc946d28bd7775f2018e348ba5e88c75c6
Thanh Phong Le <ltphongssvn@gmail.com>
2026-02-25 10:46:25 -0800

    feat: add auth enhancement DTOs - ForgotPasswordRequest, ResetPasswordRequest, MessageResponse

---
+++ backend/src/main/java/com/autoloan/backend/dto/auth/ResetPasswordRequest.java
@@ -0,0 +1,19 @@
+package com.autoloan.backend.dto.auth;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ResetPasswordRequest {
+    @NotBlank(message = "Token is required")
+    private String token;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    private String password;
+}
