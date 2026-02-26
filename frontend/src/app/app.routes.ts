d0e1714b1fabb46c332e8d6f9a4da5e432e34d1e
Thanh Phong Le <ltphongssvn@gmail.com>
2026-02-26 13:15:42 -0800

    feat: add /dashboard/loan-officer/:id route and LoanOfficerDetailComponent tests

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -6,6 +6,7 @@ export const routes: Routes = [
   { path: 'signup', loadComponent: () => import('./features/auth/signup').then(m => m.SignupComponent) },
   { path: 'dashboard', loadComponent: () => import('./features/dashboard/dashboard').then(m => m.DashboardComponent), canActivate: [authGuard] },
   { path: 'dashboard/loan-officer', loadComponent: () => import('./features/dashboard/loan-officer-dashboard').then(m => m.LoanOfficerDashboardComponent), canActivate: [authGuard] },
+  { path: 'dashboard/loan-officer/:id', loadComponent: () => import('./features/dashboard/loan-officer-detail').then(m => m.LoanOfficerDetailComponent), canActivate: [authGuard] },
   { path: 'profile', loadComponent: () => import('./features/profile/profile').then(m => m.ProfileComponent), canActivate: [authGuard] },
   { path: 'loans/new', loadComponent: () => import('./features/loan/loan-form').then(m => m.LoanFormComponent), canActivate: [authGuard] },
   { path: 'loans/:id', loadComponent: () => import('./features/loan/loan-detail').then(m => m.LoanDetailComponent), canActivate: [authGuard] },
